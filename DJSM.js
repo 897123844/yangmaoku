1e5a2d77ee854206bdb623c11c3a04b1681885f9
897123844 <40051732+897123844@users.noreply.github.com>
2021-03-25 20:40:14 +0800

    Update DJSM.js

--- DJSM.js
+++ DJSM.js
@@ -4,46 +4,7 @@
  * @Last Modified by: LXK9301
  * @Last Modified time: 2021-1-29 21:22:37
  */
-/*
-三十六计(Thirty-Six Stratagems)
 
-1.瞒天过海　crossing the sea under camouflage2
-2.围魏救赵　relieving the state of Zhao by besieging3 the state of Wei
-3.借刀杀人　killing4 someone with a borrowed knife
-4.以逸待劳　waiting at one's ease for the exhausted5 enemy
-5.趁火打劫　plundering6 a burning house
-6.声东击西　making a feint to the east and attacking in the west
-7.无中生有　creating something out of nothing
-8.暗渡陈仓　advancing secretly by an unknown path
-9.隔岸观火　watching a fire from the other side of the river
-10.笑里藏刀　covering the dagger7 with a smile
-11.李代桃僵　palming off substitute for the real thing
-12.顺手牵羊　picking up something in passing
-13.打草惊蛇　beating the grass to frighten the snake
-14.借尸还魂　resurrecting a dead soul by borrowing a corpse
-15.调虎离山　luring8 the tiger out of his den
-16.欲擒故纵　letting the enemy off in order to catch him
-17.抛砖引玉　giving the enemy something to induce him to lose more valuable things
-18.擒贼擒王　capturing the ringleader first in order to capture all the followers
-19.釜底抽薪　extracting the firewood from under the cauldron
-20.混水摸鱼　muddling9 the water to catch the fish; fishing in troubled waters
-21.金蝉脱壳　slipping away by casting off a cloak; getting away like the cicada sloughing10 its skin
-22.关门捉贼　catching11 the thief by closing / blocking his escape route
-23.远交近攻　befriending the distant enemy while attacking a nearby enemy
-24.假途伐虢　attacking the enemy by passing through a common neighbor
-25.偷梁换柱　stealing the beams and pillars and replacing them with rotten timbers
-26.指桑骂槐　reviling/ abusing the locust12 tree while pointing to the mulberry
-27.假痴不癫　feigning13 madness without becoming insane
-28.上屋抽梯　removing the ladder after the enemy has climbed up the roof
-29.树上开花　putting artificial flowers on trees
-30.反客为主　turning from the guest into the host
-31.美人计　using seductive women to corrupt14 the enemy
-32.空城计　presenting a bold front to conceal15 unpreparedness
-33.反间计　sowing discord16 among the enemy
-34.苦肉计　deceiving the enemy by torturing one's own man
-35.连环计　coordinating17 one stratagem1 with another
-36.走为上　decamping being the best; running away as the best choice
- */
 const $ = new Env('东东超市');
 //Node.js用户请在jdCookie.js处填写京东ck;
 //IOS等用户直接用NobyDa的jd cookie
@@ -52,7 +13,7 @@ let cookiesArr = [], cookie = '', jdSuperMarketShareArr = [], notify, newShareCo
 let jdNotify = false;//用来是否关闭弹窗通知，true表示关闭，false表示开启。
 let superMarketUpgrade = true;//自动升级,顺序:解锁升级商品、升级货架,true表示自动升级,false表示关闭自动升级
 let businessCircleJump = true;//小于对方300热力值自动更换商圈队伍,true表示运行,false表示禁止
-let drawLotteryFlag = false;//是否用500蓝币去抽奖，true表示开启，false表示关闭。默认关闭
+let drawLotteryFlag = true;//是否用500蓝币去抽奖，true表示开启，false表示关闭。默认关闭
 let joinPkTeam = true;//是否自动加入PK队伍
 let message = '', subTitle;
 const JD_API_HOST = 'https://api.m.jd.com/api';
